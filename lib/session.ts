e52598a34dece16782f9fd33529ff823af63be98
DerekHan <donglak.han@gmail.com>
2023-07-18 10:58:37 -0500

    get ready for the session(AUTH)

---
+++ lib/session.ts
@@ -0,0 +1,29 @@
+// save all the logged in users
+
+import { getServerSession } from "next-auth/next";
+import { NextAuthOptions, User } from "next-auth";
+import { Adapter } from "next-auth/adapters";
+import GoogleProvider from "next-auth/providers/google";
+import jsonwebtoken from "jsonwebtoken";
+import { JWT } from "next-auth/jwt";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: "",
+      clientSecret: "",
+    }),
+  ],
+  jwt: {
+    encode: ({ secret, token }) => {},
+    decode: async ({ secret, token }) => {},
+  },
+  theme: {
+    colorScheme: "light",
+    logo: "/logo.png",
+  },
+  callbacks: {
+    async session({ session }) {},
+    async signIn({ user }) {},
+  },
+};
