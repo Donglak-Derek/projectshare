f3bd6539d45b8cdad7bc613e689a2c53f567efdf
DerekHan <donglak.han@gmail.com>
2023-07-18 12:00:02 -0500

    GraphQL request setups a lot of settings

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,10 @@ import Link from "next/link";
 import Image from "next/image";
 import { NavLinks } from "@/constants";
 import AuthProviders from "./AuthProviders";
+import { getCurrentUser } from "@/lib/session";
 
-const Navbar = () => {
-  const session = null;
+const Navbar = async () => {
+  const session = await getCurrentUser();
 
   return (
     <nav className="flexBetween navbar">
@@ -23,9 +24,17 @@ const Navbar = () => {
       </div>
 
       <div>
-        {session ? (
+        {session?.user ? (
           <>
-            UserPhoto
+            {session?.user?.image && (
+              <Image
+                src={session.user.image}
+                width={40}
+                height={40}
+                className="rounded-full"
+                alt={session.user.name}
+              />
+            )}
             <Link href="/create-project">Share Work</Link>
           </>
         ) : (
--- lib/session.ts
+++ lib/session.ts
@@ -6,6 +6,7 @@ import { Adapter, AdapterUser } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
 import jsonwebtoken from "jsonwebtoken";
 import { JWT } from "next-auth/jwt";
+import { SessionInterface } from "@/common.types";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -38,3 +39,9 @@ export const authOptions: NextAuthOptions = {
     },
   },
 };
+
+export async function getCurrentUser() {
+  const session = (await getServerSession(authOptions)) as SessionInterface;
+
+  return session;
+}
